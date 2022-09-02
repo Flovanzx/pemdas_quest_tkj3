083c65ade49711c099ed4ebecc542847c73ed078
Devandry <112366608+Flovanzx@users.noreply.github.com>
2022-09-01 20:13:36 -0700

    Devandry TKJ 3
    Tugas PemDas Python 5

---
+++ Devandry-python/Python5-tkj3.py
@@ -0,0 +1,39 @@
+#Rumus Luas Persegi Sisi * Sisi
+
+sisi1 = 10
+sisi2 = 10
+luas_p = sisi1 * sisi2
+
+print("luas persegi adalah" , luas_p)
+
+
+#Rumus Luas Segitiga  A * T / 2
+
+alas_s = 20
+tinggi_s = 10
+luas_s = alas_s * tinggi_s / 2
+ 
+print("luas segitiga adalah" , luas_s)
+
+#Rumus Luas Jajar Genjang A * T
+alas_j = 10
+tinggi_j = 15
+luas_j = alas_j * tinggi_j
+
+print ("Luas Jajar Genjang Adalah" , luas_j)
+
+#Rumus Luas Lingkaran phi * r * r
+phi = 22/7
+r = 14
+luas_l = phi * r * r
+
+print ("Luas Lingkaran Adalah" , luas_l)
+
+print ("\n1.luas persegi")
+sisi = int(input("masukkan nilai sisi: "))
+luas_p = sisi * sisi
+
+print ("luas persegi adalah " , luas_p)
+
+
+
