cfd07d612cdcf27d9ff41e318e765d40469926af
Devandry <112366608+Flovanzx@users.noreply.github.com>
2022-09-04 22:05:58 -0700

    Tugas Pemdas
    Nama : Devandry Seon
    Kelas :

---
+++ python6-tkj3.py
@@ -0,0 +1,17 @@
+# Dolar Ke Rupiah
+dolar = int(input("Masukkan nilai dolar: "))
+rupiah = dolar * 15000
+
+print  ("Dolar ke Rupiah" , rupiah)
+            
+# Euro Ke Won
+euro = int(input("masukkan nilai euro: "))
+won = euro * 1357
+
+print ( "Euro Ke Won" , won )
+
+# Yen Ke Rupiah
+yen =int(input("Masukkan Nilai Yen:"))
+rupiah = yen * 106
+
+print ("Yen Ke Rupiah" , rupiah)
